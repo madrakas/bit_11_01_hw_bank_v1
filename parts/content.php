1156730818987a5467d93effcb1b08f184ee76d9
Arvydas <arvydas.simbelis@gmail.com>
2023-12-31 20:46:36 +0200

    User edit form

--- parts/content.php
+++ parts/content.php
@@ -12,6 +12,9 @@
             case 'logout':
                 require(__DIR__ . '/../sections/logout.php');
                 break;
+            case 'useredit':
+                require(__DIR__ . '/../sections/useredit.php');
+                break;
             default:
                 //consider if user is loged in when redirecting
                 require(__DIR__ . '/../sections/main.php');
