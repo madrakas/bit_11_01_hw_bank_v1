a1775f316f37c0da018a804869a475693aa0ad12
Arvydas <arvydas.simbelis@gmail.com>
2024-01-01 19:03:35 +0200

    User menu

--- parts/content.php
+++ parts/content.php
@@ -1,25 +1,43 @@
-<content>
 
-    <?php 
-        $page ?? '';
-        switch ($page) {
-            case 'signup':
-                require(__DIR__ . '/../sections/signup.php');
-                break;
-            case 'login':
-                require(__DIR__ . '/../sections/login.php');
-                break;
-            case 'logout':
-                require(__DIR__ . '/../sections/logout.php');
-                break;
-            case 'useredit':
-                require(__DIR__ . '/../sections/useredit.php');
-                break;
-            default:
-                //consider if user is loged in when redirecting
-                require(__DIR__ . '/../sections/main.php');
-                //add page not found error here
-                break;
-        }
+
+<?php 
+if (isset($_SESSION['login']) && ($_SESSION['login'] === '1' )){
+    ?>
+    <div class="left-menu">
+        <ul>
+            <li><a href="#">My money accounts</a></li>
+            <li><a href="#">Make a transaction</a></li>
+            <li><a href="#">Transactions history</a></li>
+            <li><a href="index.php?p=userview">User data</a></li>
+        </ul>
+    </div>
+    <content class="logedin">
+<?php
+}else{
+    echo '<content>';
+}
+$page ?? '';
+switch ($page) {
+    case 'signup':
+        require(__DIR__ . '/../sections/signup.php');
+        break;
+    case 'login':
+        require(__DIR__ . '/../sections/login.php');
+        break;
+    case 'logout':
+        require(__DIR__ . '/../sections/logout.php');
+        break;
+    case 'userview':
+        require(__DIR__ . '/../sections/userview.php');
+        break;
+    case 'useredit':
+        require(__DIR__ . '/../sections/useredit.php');
+        break;
+    default:
+        //consider if user is loged in when redirecting
+        require(__DIR__ . '/../sections/main.php');
+        //add page not found error here
+        break;
+}
     ?>
 </content>
---
+++ sections/userview.php
@@ -0,0 +1,26 @@
+<?php if (isset($_SESSION['login']) && ($_SESSION['login'] === '1' )){ 
+    $users = unserialize(file_get_contents(__DIR__ . '/../data/users.ser'));
+    $users = array_filter($users, fn($user) => ($user['id'] === $_SESSION['uid']));
+    $firstname = $users[0]['firstname'] ?? '';
+    $lastname = $users[0]['lastname'] ?? '';
+    $email = $users[0]['email'] ?? '';
+    $ak = $users[0]['ak'] ?? '';
+
+} else {
+    header("HTTP/1.1 401 Unauthorized");
+    die;
+}?>
+
+
+<h1>Your Bit Bank account</h1>
+    <p><strong>First name: </strong> <?= $firstname?>.</p>
+    <p><strong>Last name: </strong><?= $lastname?>.</p>
+    <p><strong>Personal identification number: </strong> <?= $ak?>.</p>
+    <p><strong>Email address: </strong> <?= $email?>.</p>
+    
+
+    <h2>Change your data</h2>
+    <p><a href="index.php?p=useredit">Edit personal information</a></p>
+    <p><a href="index.php?p=userchangepw">Change password</a></p>
+    <p><a href="index.php?p=usercloseacc">Close acount</a></p>
+
