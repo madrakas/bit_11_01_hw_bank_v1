4dbf5da36c7d1b4e7aa33149664b44cf076b5f4e
Arvydas <arvydas.simbelis@gmail.com>
2024-01-01 19:47:34 +0200

    Change user pw Form

--- process/users/editpw.php
+++ process/users/editpw.php
@@ -51,10 +51,10 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
             
             $_SESSION['msg'] = 'Error: ' . $err;
             $_SESSION['msgType'] = 'red';
-            // echo 'Klaida: ' . $err;
-            // echo '<pre>';
-            // print_r($users);
-            // echo '</pre>';
+            echo 'Klaida: ' . $err;
+            echo '<pre>';
+            print_r($users);
+            echo '</pre>';
             // Go back to form page
             
             header('location: http://localhost/bank/index.php?p=userchangepw');
