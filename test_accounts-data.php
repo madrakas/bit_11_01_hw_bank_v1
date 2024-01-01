7ea116530e1c4fb4d438252622b79c28106dd322
Arvydas <arvydas.simbelis@gmail.com>
2024-01-01 21:16:24 +0200

    Accounts data and seeder

---
+++ test_accounts-data.php
@@ -0,0 +1,11 @@
+<?php
+
+$accounts = unserialize(file_get_contents(__DIR__ . '/data/acounts.ser'));
+$acountsMaxID = unserialize(file_get_contents(__DIR__ . '/data/accounts-max-id.ser'));
+
+
+echo '<pre>';
+print_r($accounts);
+print_r($acountsMaxID);
+echo '<pre>';
+?>
