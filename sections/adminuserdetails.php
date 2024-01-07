ac6ac8a9ecb7884597aad4e848159aac9b7f66cd
Arvydas <arvydas.simbelis@gmail.com>
2024-01-07 13:01:02 +0200

    Admin can view users and their accounts

--- sections/adminuserdetails.php
+++ sections/adminuserdetails.php
@@ -6,14 +6,14 @@
                 $users = unserialize(file_get_contents(__DIR__ . '/../data/users.ser'));
                 $userExists = false;
                 foreach($users as $user) {
-                    if ($user['id'] === strval($_GET['usr'])){
+
+                    if (strval($user['id']) === strval($_GET['usr'])){
                         $userExists = true;
-                        $usrid = $user['id'];
+                        $uid = $user['id'];
                         $ufname = $user['firstname'];
                         $ulname = $user['lastname'];
                         $uak = $user['ak'];
                         $uemail = $user['email'];
-                        echo ($uak);
                     }
                 }
                 if (!$userExists){
@@ -32,12 +32,34 @@
         header('Location: http://localhost/bank/index.php');
         die;
     }
-
 ?>
 
 <h1>User Details</h1>
 <?php
     echo '<div class="acc_row">';
-    echo '<div>ID: ' .  $usrid . '</div>';
+    echo '<div>ID: ' .  $uid . '</div>';
     echo '</div>';
+    echo '<div class="details-head">Personal data</div>';
+    echo '<div class="detail-row">';
+    echo '<div><p><strong>First Name: </strong>'. $ufname .'</p>';
+    echo '<p><strong>Last Name: </strong>'. $ulname .'</p>';
+    echo '<p><strong>Email Name: </strong>'. $uemail .'</p>';
+    echo '<p><strong>Personal identificartion code: </strong>'. $uak .'</p>';
+    echo '<p><a href="#">Edit data</a> <a href="#">Delete user</a></p></div>';
+    echo '</div>';
+    echo '<div class="details-head">Money accounts</div>';
+
+    $accounts = unserialize(file_get_contents(__DIR__ . '/../data/accounts.ser'));
+    $accounts = array_filter($accounts, fn($account) => $account['uid'] === $uid);
+    $i = 0;
+    foreach ($accounts as $account) {
+        echo '<div class="detail-row">';
+        echo '<div>' . ++$i . '.</div>';
+        echo '<div>' .  $account['iban'] . ' | ' . $account['amount'] . ' ' . $account['currency'] . ' | <a href="#">Add funds</a> | <a href="#">Withdraw funds</a> | <a href="#">View transactions</a> | <a href="#">Delete money account</a></div>';
+        echo '</div>';}
+
+        echo '<pre>';
+    print_r ($accounts);
+    echo '</pre>';
 ?>
+
