5e95fcf73da8e4f6bd80015adeec465b01c7e826
Arvydas <arvydas.simbelis@gmail.com>
2024-01-09 00:10:53 +0200

    Admin deletes accounts

--- process/accounts/delete.php
+++ process/accounts/delete.php
@@ -5,20 +5,40 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         die;
 } else {
     session_start();
+
     if (isset($_SESSION['login']) && ($_SESSION['login'] === '1' && isset($_POST['acc']) )){ 
         $accid = intval($_POST['acc']);
-        // Check if user owns account
+        $notMyAcc = false;
         $accounts = unserialize(file_get_contents(__DIR__ . '/../../data/accounts.ser'));
-        $accounts = array_filter($accounts, fn($account) => ($account['uid'] === $_SESSION['uid']));
+        $accounts = array_filter($accounts, fn($account) => ($account['id'] === $accid));
+        $accounts = array_values($accounts);
+        $accUid = $accounts[0]['uid'];
+
+        if ($accUid !== $_SESSION['uid']){   // user doesn't own account
+            $admins = unserialize(file_get_contents(__DIR__ . '/../../data/admins.ser'));
+            if (in_array($_SESSION['uid'], $admins)){
+                $isAdmin = true;   // if user is admin we may continue
+                $notMyAcc =true;
+            }else{
+                header('Location: http://localhost/bank/index.php');   // dead end if not admin
+                die;
+            }
+        } else { // user owns account
+
+        }
+
+        // at this point acount either belongs to user either user is admin, so we are good
+        $accounts = unserialize(file_get_contents(__DIR__ . '/../../data/accounts.ser'));
+        $accounts = array_filter($accounts, fn($account) => ($account['uid'] === $accUid));
         $err= '';
         if (count($accounts) < 2){
             $err .= 'Cannot delete. User must have at least 1 account.<br/>';
         }
+
+        $accounts = unserialize(file_get_contents(__DIR__ . '/../../data/accounts.ser'));
         $accounts = array_filter($accounts, fn($account) => ($account['id'] === $accid));
         $accounts = array_values($accounts);
-        if (count($accounts) === 0){
-            $err .= 'Account not found.<br/>';
-        } elseif($accounts[0]['amount'] !== 0){     //Check if account is empty
+        if ($accounts[0]['amount'] !== 0){     //Check if account is empty
             $err .= 'Canot delete - account is not empty (' . $accounts[0]['amount'] . ').<br/>';
         }
 
@@ -26,8 +46,8 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
             $_SESSION['msg'] = 'Error: ' . $err ;
             $_SESSION['msgType'] = 'red';
             header('Location: http://localhost/bank/index.php?p=accountsview');
+            die;
         }
-        
 
         $accounts = unserialize(file_get_contents(__DIR__ . '/../../data/accounts.ser'));
     
@@ -38,16 +58,18 @@ if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
         }
 
         $accounts = array_values($accounts);
-        
-        // echo '<pre>';
-        // print_r($accounts);
-        // echo '</pre>';
         file_put_contents(__DIR__ . '/../../data/accounts.ser', serialize($accounts));
 
         $_SESSION['msg'] = 'Money account deleted successfully.';
         $_SESSION['msgType'] = 'green';
 
-        header('location: http://localhost/bank/index.php?p=accountsview');
+        if ($notMyAcc){
+            header('location: http://localhost/bank/index.php?p=adminuserdetails&usr=' . $accUid);
+            die;
+        }else{
+            header('location: http://localhost/bank/index.php?p=accountsview');
+            die;
+        }
     }else{
         header('location: http://localhost/bank/index.php');
         die;
--- sections/accountsdel.php
+++ sections/accountsdel.php
@@ -1,31 +1,61 @@
 <?php if (isset($_SESSION['login']) && ($_SESSION['login'] === '1' )){ 
        if (isset($_GET['acc'])){
         $accid = intval($_GET['acc']);
+        
         // Check if user owns account
         $accounts = unserialize(file_get_contents(__DIR__ . '/../data/accounts.ser'));
-        $accounts = array_filter($accounts, fn($account) => ($account['uid'] === $_SESSION['uid']));
+        $accounts = array_filter($accounts, fn($account) => ($account['id'] === $accid));
+        $accounts = array_values($accounts);
+        $accUid = $accounts[0]['uid'];
+        if ($accUid !== $_SESSION['uid']){   // user doesn't own account
+            $admins = unserialize(file_get_contents(__DIR__ . '/../data/admins.ser'));
+            if (in_array($_SESSION['uid'], $admins)){
+                $isAdmin = true;   // if user is admin we may continue
+            }else{
+                header('Location: http://localhost/bank/index.php');   // dead end if not admin
+                die;
+            }
+        } else { // user owns account
+
+        }
+        
+        // at this point acount either belongs to user either user is admin, so we are good
+        $accounts = unserialize(file_get_contents(__DIR__ . '/../data/accounts.ser'));
+        $accounts = array_filter($accounts, fn($account) => ($account['uid'] === $accUid));
         $err= '';
         if (count($accounts) < 2){
             $err .= 'Cannot delete. User must have at least 1 account.<br/>';
         }
+        
+        $accounts = unserialize(file_get_contents(__DIR__ . '/../data/accounts.ser'));
         $accounts = array_filter($accounts, fn($account) => ($account['id'] === $accid));
         $accounts = array_values($accounts);
-        if (count($accounts) === 0){
-            $err .= 'Account not found.<br/>';
-        } elseif($accounts[0]['amount'] !== 0){     //Check if account is empty
+        if ($accounts[0]['amount'] !== 0){     //Check if account is empty
             $err .= 'Canot delete - account is not empty (' . $accounts[0]['amount'] . ').<br/>';
         }
 
         if ($err !== ''){
             $_SESSION['msg'] = 'Error: ' . $err ;
             $_SESSION['msgType'] = 'red';
+            // echo '<pre>';
+            // print_r ($accounts);
+            // echo '</pre>';
+            // echo $accounts[0]['amount'];
             header('Location: http://localhost/bank/index.php?p=accountsview');
+            die;
         }
+       }else{
+        header('Location: http://localhost/bank/index.php');
+        die;
        }
+    
+    
     } else {
         header('Location: http://localhost/bank/index.php');
-    die;
-}?>
+        die;
+    }
+    
+    ?>
 
 <h1>Delete money account <?= $accounts[0]['iban']?>?</h1>
 <form action="http://localhost/bank/process/accounts/delete.php" method="post">
